d3bea84068987cabda695ce6e50b791335d99139
Sheila <s.hopper01110@gmail.com>
2019-03-13 18:45:15 -0400

    I'm not sure what this is

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('should call the speak function passed as prop', () => {
+    const toggleLocked = jest.fn();
+
+    const { getByText } = render(<Controls toggleLocked={toggleLocked} />);
+
+    fireEvent.click(getByText(/toggleLocked/i));
+    expect(speak).toHaveBeenCalled();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+
+import Speaker from './Speaker';
+
+describe('<Dashboard />', () => {
+  it('should call the speak function passed as prop', () => {
+    const speak = jest.fn();
+
+    const { getByText } = render(<Speaker speak={speak} />);
+
+    fireEvent.click(getByText(/speak/i));
+    expect(speak).toHaveBeenCalled();
+  });
+});
